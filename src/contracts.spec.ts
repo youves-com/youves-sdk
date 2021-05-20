14859ceb6904490c9f7942b08e7f2b6d7bc0e30a
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-05-20 15:47:40 +0200

    feat(contracts): add mainnet and florencenet contracts

--- src/contracts.spec.ts
+++ src/contracts.spec.ts
@@ -1,6 +1,6 @@
 import { TezosToolkit } from "@taquito/taquito";
 import { importKey } from "@taquito/signer";
-import { Youves } from "./contracts";
+import { contracts, Youves } from "./public";
 import BigNumber from "bignumber.js";
 
 const TIMEOUT = 1000*60*2 // 2 min timeout, because 1 min blocktime
@@ -42,7 +42,7 @@ importKey(
 
 if(false){
 test("should create a vault", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const amount = 20*10**6 //pay 1 tez
   const mintAmount = 10**12 //mint 1 uUSD
   const result = await youves.createVault(amount, mintAmount)
@@ -50,70 +50,70 @@ test("should create a vault", async () => {
 }, TIMEOUT)
 
 test("should fund vault", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const amount = 1*10**6 //pay 1 tez
   const result = await youves.fundVault(amount)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should mint", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const mintAmount = 10**12 
   const result = await youves.mint(mintAmount)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should transfer minted tokens", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const tokenAmount = 10**11 //pay 1 tez
   const result = await youves.transferSyntheticToken(DEFAULT_RECIPIENT, tokenAmount)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should claim governance tokens", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.claimGovernanceToken()
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should add staking pool as governance token operator", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.addGovernanceTokenOperator(youves.REWARD_POOL_ADDRESS)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should deposit to rewards pool", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.depositToRewardsPool(89222)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should claim staking rewards", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.claimRewards()
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should withdraw from rewards pool", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.withdrawFromRewardsPool()
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should add synthetic token operator", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.addSynthenticTokenOperator(youves.SAVINGS_POOL_ADDRESS)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should deposit to savings pool", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.depositToSavingsPool(10**11)
   expect(result.length).toBe(51)
 }, TIMEOUT)
 
 test("should withdraw from savings pool", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.withdrawFromSavingsPool()
   expect(result.length).toBe(51)
 }, TIMEOUT)
@@ -121,13 +121,13 @@ test("should withdraw from savings pool", async () => {
 
 // quipo tests
 test("should trade tez for synthetic token", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.tezToSyntheticSwap(10**5,1)
   expect(result.length).toBe(51)
 }, TIMEOUT*2)
 
 test("should trade synthetic token for tez", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   const result = await youves.syntheticAssetToTezSwap(10**3,1)
   expect(result.length).toBe(51)
 }, TIMEOUT*2)
@@ -141,7 +141,7 @@ async function verifyBigNumberGteZero(result: Promise<BigNumber>):Promise<Boolea
 }
 
 test("should get the required numbers", async () => {
-  const youves = new Youves(toolkit)
+  const youves = new Youves(toolkit, contracts.florencenet)
   await verifyBigNumberGteZero(youves.getTotalSyntheticAssetSupply())
   await verifyBigNumberGteZero(youves.getSyntheticAssetExchangeRate())
   await verifyBigNumberGteZero(youves.getGovernanceTokenExchangeRate())

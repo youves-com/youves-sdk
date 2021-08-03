82ad83542e54e509fb614b98e1465234da7ef5fa
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-08-03 11:42:58 +0200

    feat(test): format ts file

--- src/contracts.spec.ts
+++ src/contracts.spec.ts
@@ -58,237 +58,236 @@ const indexerUrl = 'https://youves-indexer.dev.gke.papers.tech/v1/graphql'
 
 importKey(toolkit, FAUCET_KEY.email, FAUCET_KEY.password, FAUCET_KEY.mnemonic.join(' '), FAUCET_KEY.secret).catch((e) => console.error(e))
 
-if(false){
-test(
-  'should create a vault',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const amount = 10 * 10 ** 6 //pay 10 tez
-    const mintAmount = 10 ** 12 //mint 1 uUSD
-    const result = await youves.createVault(amount, mintAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+if (false) {
+  test(
+    'should create a vault',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const amount = 10 * 10 ** 6 //pay 10 tez
+      const mintAmount = 10 ** 12 //mint 1 uUSD
+      const result = await youves.createVault(amount, mintAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should fund vault',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const amount = 10 * 10 ** 6 //pay 10 tez
-    const result = await youves.fundVault(amount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should fund vault',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const amount = 10 * 10 ** 6 //pay 10 tez
+      const result = await youves.fundVault(amount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should mint',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const mintAmount = 10 ** 12
-    const result = await youves.mint(mintAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should mint',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const mintAmount = 10 ** 12
+      const result = await youves.mint(mintAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should burn',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const mintAmount = 10 ** 10
-    const result = await youves.burn(mintAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should burn',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const mintAmount = 10 ** 10
+      const result = await youves.burn(mintAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should withdraw',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const mutezAmount = 10 ** 5
-    const result = await youves.withdrawCollateral(mutezAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should withdraw',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const mutezAmount = 10 ** 5
+      const result = await youves.withdrawCollateral(mutezAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should transfer minted tokens',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const tokenAmount = 10 ** 10 //pay 1 tez
-    const result = await youves.transferSyntheticToken(DEFAULT_RECIPIENT, tokenAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should transfer minted tokens',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const tokenAmount = 10 ** 10 //pay 1 tez
+      const result = await youves.transferSyntheticToken(DEFAULT_RECIPIENT, tokenAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should claim governance tokens',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.claimGovernanceToken()
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should claim governance tokens',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.claimGovernanceToken()
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should transfer governance tokens',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const tokenAmount = 10 * 10 ** 12 //pay 1 tez
-    const result = await youves.transferGovernanceToken(DEFAULT_RECIPIENT, tokenAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should transfer governance tokens',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const tokenAmount = 10 * 10 ** 12 //pay 1 tez
+      const result = await youves.transferGovernanceToken(DEFAULT_RECIPIENT, tokenAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should add staking pool as governance token operator',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.addGovernanceTokenOperator(youves.REWARD_POOL_ADDRESS)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should add staking pool as governance token operator',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.addGovernanceTokenOperator(youves.REWARD_POOL_ADDRESS)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should deposit to rewards pool',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.depositToRewardsPool(89222)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should deposit to rewards pool',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.depositToRewardsPool(89222)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should claim staking rewards',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.claimRewards()
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should claim staking rewards',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.claimRewards()
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should withdraw from rewards pool',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.withdrawFromRewardsPool()
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should withdraw from rewards pool',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.withdrawFromRewardsPool()
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should add synthetic token operator',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.addSynthenticTokenOperator(youves.SAVINGS_POOL_ADDRESS)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should add synthetic token operator',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.addSynthenticTokenOperator(youves.SAVINGS_POOL_ADDRESS)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should deposit to savings pool',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.depositToSavingsPool(10 ** 11)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should deposit to savings pool',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.depositToSavingsPool(10 ** 11)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should withdraw from savings pool',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.withdrawFromSavingsPool()
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should withdraw from savings pool',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.withdrawFromSavingsPool()
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should deposit to savings pool (again)',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.depositToSavingsPool(10 ** 11)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should deposit to savings pool (again)',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.depositToSavingsPool(10 ** 11)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should mint new tokens ',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const mintAmount = 10 ** 12
-    const result = await youves.mint(mintAmount)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should mint new tokens ',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const mintAmount = 10 ** 12
+      const result = await youves.mint(mintAmount)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should create a new intent',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.advertiseIntent(10 ** 11)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should create a new intent',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.advertiseIntent(10 ** 11)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'should partially fulfill intent',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.fulfillIntent(FAUCET_KEY.pkh, (10 ** 10))
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  test(
+    'should partially fulfill intent',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.fulfillIntent(FAUCET_KEY.pkh, 10 ** 10)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
+  test(
+    'removes intent',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.removeIntent()
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT
+  )
 
-test(
-  'removes intent',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.removeIntent()
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT
-)
+  // quipo tests
+  test(
+    'should trade tez for synthetic token',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.tezToSyntheticSwap(10 ** 5, 1)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT * 2
+  )
 
-// quipo tests
-test(
-  'should trade tez for synthetic token',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.tezToSyntheticSwap(10 ** 5, 1)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT * 2
-)
-
-test(
-  'should trade synthetic token for tez',
-  async () => {
-    const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
-    const result = await youves.syntheticAssetToTezSwap(10 ** 3, 1)
-    expect(result.length).toBe(51)
-  },
-  TIMEOUT * 2
-)
+  test(
+    'should trade synthetic token for tez',
+    async () => {
+      const youves = new Youves(toolkit, contracts.florencenet, new MemoryStorage(), indexerUrl)
+      const result = await youves.syntheticAssetToTezSwap(10 ** 3, 1)
+      expect(result.length).toBe(51)
+    },
+    TIMEOUT * 2
+  )
 }
 
 async function testBigNumberGteZero(tag: string, result: Promise<BigNumber>) {
@@ -331,7 +330,7 @@ testBigNumberGteZero('getGovernanceTokenExchangeMaximumTezAmount', youves.getGov
 testBigNumberGteZero('getGovernanceTokenExchangeMaximumTokenAmount', youves.getGovernanceTokenExchangeMaximumTokenAmount())
 testBigNumberGteZero('getSyntheticAssetExchangeMaximumTezAmount', youves.getSyntheticAssetExchangeMaximumTezAmount())
 testBigNumberGteZero('getSyntheticAssetExchangeMaximumTokenAmount', youves.getSyntheticAssetExchangeMaximumTokenAmount())
-testBigNumberGteZero('getOwnSavingsPoolStake',youves.getOwnSavingsPoolStake())
+testBigNumberGteZero('getOwnSavingsPoolStake', youves.getOwnSavingsPoolStake())
 // testing the indexer values
 testBigNumberGteZero('getTotalBalanceInVaults', youves.getTotalBalanceInVaults())
 testBigNumberGteZero('getVaultCount', youves.getVaultCount())
@@ -341,13 +340,13 @@ testBigNumberGteZero('getTotalCollateralRatio', youves.getTotalCollateralRatio()
 // testing intent list
 test('should get all intents', async () => {
   const awaitedResult = await youves.getIntents()
-  console.log("intents", awaitedResult)
+  console.log('intents', awaitedResult)
   expect(awaitedResult.length).toBeGreaterThan(0)
   return true
 })
 
 test('should get all intents', async () => {
   const awaitedResult = await youves.getFullfillableIntents()
-  console.log("intents", awaitedResult)
+  console.log('intents', awaitedResult)
   return true
 })

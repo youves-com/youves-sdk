bf3a0a541e35e29632979a4a4e8505ffa1174564
Tim Schluchter <t.schluchter@papers.ch>
2021-09-29 10:13:36 +0200

    fix(contracts): check for uusd

--- src/contracts.ts
+++ src/contracts.ts
@@ -854,18 +854,22 @@ export class Youves {
 
   @cache()
   public async getAllowsSettlement(): Promise<boolean | undefined> {
-    return this.getFromStorageOrPersist(StorageKey.ALLOWS_SETTLEMENT, async () => {
-      const source = await this.getOwnAddress()
-      const engineContract = await this.engineContractPromise
-      const storage = (await this.getStorageOfContract(engineContract)) as any
-      const vaultContext = await this.getStorageValue(storage, 'vault_contexts', source)
+    if (this.ENGINE_TYPE !== EngineType.TRACKER_V2) {
+      return true
+    } else {
+      return this.getFromStorageOrPersist(StorageKey.ALLOWS_SETTLEMENT, async () => {
+        const source = await this.getOwnAddress()
+        const engineContract = await this.engineContractPromise
+        const storage = (await this.getStorageOfContract(engineContract)) as any
+        const vaultContext = await this.getStorageValue(storage, 'vault_contexts', source)
 
-      if (!vaultContext) {
-        throw new Error('Account does not have a Vault yet!')
-      }
+        if (!vaultContext) {
+          throw new Error('Account does not have a Vault yet!')
+        }
 
-      return vaultContext.allows_settlement
-    })
+        return vaultContext.allows_settlement
+      })
+    }
   }
 
   @cache()

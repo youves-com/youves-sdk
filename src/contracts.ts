640e545018c1d178ea3fc506a4838060da073e81
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-11-03 17:13:36 +0100

    fix(udefi): observed price

--- src/contracts.ts
+++ src/contracts.ts
@@ -673,7 +673,16 @@ export class Youves {
 
   @cache()
   public async getSyntheticAssetExchangeRate(): Promise<BigNumber> {
-    return new QuipuswapExchange(this.tezos, this.contracts.DEX[0].address, this.tokens.xtzToken, this.token).getExchangeRate()
+    if (this.collateralToken.symbol === 'tez') {
+      return await new QuipuswapExchange(this.tezos, this.contracts.DEX[0].address, this.tokens.xtzToken, this.token).getExchangeRate()
+    } else {
+      return new PlentyExchange(
+        this.tezos,
+        this.contracts.DEX[1].address,
+        this.contracts.DEX[1].token1,
+        this.contracts.DEX[1].token2
+      ).getExchangeRate()
+    }
   }
 
   // TODO: Can we replace this with the Quipuswap class?
@@ -700,14 +709,6 @@ export class Youves {
 
   @cache()
   public async getObservedPrice(): Promise<BigNumber> {
-    console.log(
-      'getObservedPrice',
-      this.token.symbol,
-      new BigNumber(1)
-        .dividedBy(await this.getSyntheticAssetExchangeRate())
-        .multipliedBy(10 ** this.collateralToken.decimals)
-        .toString()
-    )
     if (this.ENGINE_TYPE === EngineType.TRACKER_V1) {
       return new BigNumber(1).dividedBy(await this.getSyntheticAssetExchangeRate()).multipliedBy(10 ** 6)
     } else {

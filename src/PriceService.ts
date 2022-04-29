ab053124f401e8fb7cb442514cc90e22958f5b08
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-04-29 08:06:03 -0300

    feat(): update price service

--- src/PriceService.ts
+++ src/PriceService.ts
@@ -9,18 +9,66 @@ export function getPriceService(): PriceService {
   return service
 }
 
+type TokenKey = string // `${TokenSymbol}-${TokenSymbol}`
+
 class PriceService {
-  private prices: Map<TokenSymbol, BigNumber> = new Map()
+  public prices: Map<TokenKey, BigNumber> = new Map()
 
   constructor() {}
 
-  async getUSDPrice(fromToken: TokenSymbol): Promise<BigNumber> {
-    return this.prices.get(fromToken) ?? new BigNumber(0)
+  async printPrice(fromToken: TokenSymbol, toToken: TokenSymbol) {
+    console.log('1', fromToken, 'will give you', (await this.getTokenToTokenPrice(fromToken, toToken)).toString(), toToken)
+    console.log('1', toToken, 'will give you', (await this.getTokenToTokenPrice(toToken, fromToken)).toString(), fromToken)
   }
 
-  async getTokenToTokenPrice(fromToken: TokenSymbol, toToken: TokenSymbol): Promise<BigNumber> {
-    const from = this.prices.get(fromToken) ?? new BigNumber(1)
-    const to = this.prices.get(toToken) ?? new BigNumber(1)
-    return from.div(to)
+  // async fetchPrice(fromToken: TokenSymbol, toToken: TokenSymbol): Promise<void> {
+
+  //   this.prices.set(`${fromToken}-${toToken}`, price)
+  //   this.prices.set(`${toToken}-${fromToken}`, new BigNumber(1).div(price))
+  // }
+
+  async addPrice(fromToken: TokenSymbol, toToken: TokenSymbol, price: BigNumber): Promise<void> {
+    this.prices.set(`${fromToken}-${toToken}`, price)
+    this.prices.set(`${toToken}-${fromToken}`, new BigNumber(1).div(price))
+    this.printPrice(fromToken, toToken)
+  }
+
+  async getUSDPrice(fromToken: TokenSymbol): Promise<BigNumber> {
+    return this.prices.get(`${fromToken}-uUSD`) ?? new BigNumber(0)
+  }
+
+  async getTokenToTokenPrice(fromToken: TokenSymbol, toToken: TokenSymbol, onlyExact: boolean = false): Promise<BigNumber> {
+    // const from = this.prices.get(`${fromToken}-${toToken}`) ?? new BigNumber(1)
+    // const to = this.prices.get(toToken) ?? new BigNumber(1)
+    // return from.div(to)
+    let price = this.prices.get(`${fromToken}-${toToken}`)
+    if (price) {
+      return price
+    }
+
+    if (onlyExact) {
+      console.log('NOT FOUND', fromToken, toToken)
+      return new BigNumber(-2)
+    }
+
+    if (fromToken !== 'tez' && toToken !== 'tez') {
+      price = await this.findRoute(fromToken, toToken, 'tez')
+      if (price) {
+        return price
+      }
+    }
+
+    if (fromToken !== 'uUSD' && toToken !== 'uUSD') {
+      price = await this.findRoute(fromToken, toToken, 'uUSD')
+      if (price) {
+        return price
+      }
+    }
+
+    return new BigNumber(-1)
+  }
+
+  async findRoute(fromToken: TokenSymbol, toToken: TokenSymbol, hopToken: TokenSymbol) {
+    return (await this.getTokenToTokenPrice(fromToken, hopToken, true)).times(await this.getTokenToTokenPrice(hopToken, toToken, true))
   }
 }

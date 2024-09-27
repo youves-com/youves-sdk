717a7eb3263e8f23f34f818ace825110f2124c7a
Lorenzo Spoleti <l.spoleti@papers.ch>
2024-09-27 15:03:55 +0200

    feat(): back to new pool 🙂

--- src/staking/bailout-pool.ts
+++ src/staking/bailout-pool.ts
@@ -200,7 +200,7 @@ export class BailoutPool {
   async getTransactionValueInTimeframe(from: Date, to: Date): Promise<BigNumber> {
     const indexer = new YouvesIndexer(this.indexerConfig)
 
-    const pool = 'KT1PL1YciLdwMbydt21Ax85iZXXyGSrKT2BE'
+    const pool = 'KT1XFnhsV8Yd5FaaZY4ktR7Qt8fBMdxgZ6qh'
     // This is the pool where YOUs are swapped and sent to the unified staking contract. Currently, this is the only source of rewards. In the future, we might have to filter for multiple senders.
 
     return indexer.getTransferAggregateOverTime(this.stakingContract, this.rewardToken, from, to, pool)
